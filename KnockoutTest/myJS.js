edafd9848cc54d05373d10b6b30d67559318c887
orhankj <orhan_kj@abv.bg>
2015-09-30 13:44:09 +0300

    KnockoutJS
    knockout jquery hardcored

---
+++ KnockoutTest/myJS.js
@@ -0,0 +1,314 @@
+﻿var data = [
+  {
+      "_id": "5608ef7db3723785000cc555",
+      "index": 0,
+      "guid": "fc4c666f-3da1-4627-899b-a79fea0901cd",
+      "isActive": false,
+      "balance": "$3,288.11",
+      "age": 22,
+      "eyeColor": "brown",
+      "name": {
+          "first": "Brandie",
+          "last": "Mathis"
+      },
+      "company": "XEREX",
+      "email": "brandie.mathis@xerex.com",
+      "phone": "+1 (975) 575-2608",
+      "address": "728 Colonial Road, Neibert, South Dakota, 7145",
+      "about": "Ad pariatur aute adipisicing magna enim nulla laboris. Sunt officia consequat elit fugiat ipsum sint aliquip sit irure sunt quis laboris. Labore elit consectetur excepteur ex in velit quis mollit excepteur aute nostrud cupidatat sint. Veniam labore incididunt exercitation mollit veniam et consequat mollit dolore do. Laboris ad proident et culpa irure et ullamco qui fugiat labore ex voluptate culpa. Laboris nisi sunt magna et.",
+      "registered": "Tuesday, August 5, 2014 3:53 PM",
+      "tags": [
+        "deserunt",
+        "do",
+        "sint",
+        "proident",
+        "nulla",
+        "Lorem",
+        "esse",
+        "custom_tag"
+      ],
+      "range": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        7,
+        8
+      ],
+      "greeting": "Hello, Brandie! You have 7 unread messages.",
+      "favoriteFruit": "strawberry"
+  },
+  {
+      "_id": "5608ef7d5503c0da99a874ae",
+      "index": 1,
+      "guid": "5ca9f2f0-a2bf-4ba5-be84-40df2020c7e6",
+      "isActive": false,
+      "balance": "$2,205.31",
+      "age": 20,
+      "eyeColor": "brown",
+      "name": {
+          "first": "Celia",
+          "last": "Hopper"
+      },
+      "company": "QUANTALIA",
+      "email": "celia.hopper@quantalia.ca",
+      "phone": "+1 (899) 588-2155",
+      "address": "488 Monaco Place, Abrams, Oklahoma, 4175",
+      "about": "Magna ut cillum nisi ipsum tempor aute. Mollit exercitation voluptate cillum anim sit laborum qui ullamco occaecat culpa in. Consequat eu qui sint exercitation magna ea proident excepteur ullamco velit nisi. Ullamco ullamco anim cillum Lorem ea mollit do minim. Ad nisi dolor consectetur anim nisi ex ea elit ullamco nostrud nisi id.",
+      "registered": "Saturday, January 3, 2015 10:56 PM",
+      "tags": [
+        "deserunt",
+        "do",
+        "sint",
+        "proident",
+        "nulla",
+        "Lorem",
+        "esse",
+        "custom_tag"
+      ],
+      "range": [
+        0,
+        1,
+        3,
+        4,
+        6,
+        7,
+        9
+      ],
+      "greeting": "Hello, Celia! You have 6 unread messages.",
+      "favoriteFruit": "strawberry"
+  },
+  {
+      "_id": "5608ef7de2bda03d499a58ad",
+      "index": 2,
+      "guid": "f05670bb-f7a3-4bfa-b42a-1d1f3a84e840",
+      "isActive": false,
+      "balance": "$1,860.33",
+      "age": 32,
+      "eyeColor": "brown",
+      "name": {
+          "first": "Kay",
+          "last": "Hickman"
+      },
+      "company": "ORONOKO",
+      "email": "kay.hickman@oronoko.me",
+      "phone": "+1 (966) 452-3359",
+      "address": "662 Lawrence Street, Olney, District Of Columbia, 4541",
+      "about": "Magna labore pariatur enim officia dolor tempor quis ut et pariatur id deserunt amet voluptate. Et occaecat sunt ut aute consectetur. Sint sunt qui exercitation ex ipsum. Commodo et incididunt quis ipsum culpa elit et mollit. Dolor deserunt voluptate non aliquip amet enim ut ullamco sunt nostrud.",
+      "registered": "Thursday, March 20, 2014 11:00 PM",
+      "tags": [
+        "deserunt",
+        "do",
+        "sint",
+        "proident",
+        "nulla",
+        "Lorem",
+        "esse",
+        "custom_tag"
+      ],
+      "range": [
+        1,
+        2,
+        4,
+        5,
+        8,
+        9,
+        2
+      ],
+      "greeting": "Hello, Kay! You have 7 unread messages.",
+      "favoriteFruit": "banana"
+  },
+  {
+      "_id": "5608ef7d5d135a36308502eb",
+      "index": 3,
+      "guid": "1db3d1c1-fbbe-488a-a879-879550c0fba4",
+      "isActive": false,
+      "balance": "$1,150.18",
+      "age": 36,
+      "eyeColor": "brown",
+      "name": {
+          "first": "Angelique",
+          "last": "Payne"
+      },
+      "company": "ACCRUEX",
+      "email": "angelique.payne@accruex.biz",
+      "phone": "+1 (899) 504-3723",
+      "address": "103 Woodpoint Road, Choctaw, Indiana, 2114",
+      "about": "Aute ad culpa cupidatat ipsum nisi est anim Lorem. Amet proident minim eiusmod minim. Ipsum laboris ea do cupidatat eiusmod ipsum ad sint mollit consequat. Labore quis id eiusmod elit eu cupidatat aliqua reprehenderit veniam consectetur. Consequat amet veniam id aliquip cupidatat officia sint.",
+      "registered": "Friday, February 20, 2015 8:57 AM",
+      "tags": [
+        "deserunt",
+        "do",
+        "sint",
+        "proident",
+        "nulla",
+        "Lorem",
+        "esse",
+        "custom_tag"
+      ],
+      "range": [
+        4,
+        5,
+        6,
+        7,
+        8,
+        7,
+        8
+
+      ],
+      "greeting": "Hello, Angelique! You have 9 unread messages.",
+      "favoriteFruit": "strawberry"
+  },
+  {
+      "_id": "5608ef7dac4c05dfc53134ab",
+      "index": 4,
+      "guid": "614a1634-ac6f-441b-816b-966ad4b7581b",
+      "isActive": true,
+      "balance": "$2,814.13",
+      "age": 26,
+      "eyeColor": "blue",
+      "name": {
+          "first": "Madeline",
+          "last": "Erickson"
+      },
+      "company": "PHUEL",
+      "email": "madeline.erickson@phuel.co.uk",
+      "phone": "+1 (942) 449-2811",
+      "address": "232 Howard Place, Riverton, Ohio, 9913",
+      "about": "Dolore occaecat excepteur consectetur quis esse qui est deserunt enim cillum eu nulla. Sunt irure est occaecat fugiat deserunt irure fugiat sint incididunt veniam. Ex labore excepteur reprehenderit sit dolor tempor sunt tempor. Do enim aliqua consectetur sint veniam ex deserunt officia veniam exercitation.",
+      "registered": "Saturday, June 14, 2014 5:24 AM",
+      "tags": [
+        "deserunt",
+        "do",
+        "sint",
+        "proident",
+        "nulla",
+        "Lorem",
+        "esse",
+        "custom_tag"
+      ],
+      "range": [
+        1,
+        2,
+        3,
+        4,
+        8,
+        9,
+        9
+      ],
+      "greeting": "Hello, Madeline! You have 8 unread messages.",
+      "favoriteFruit": "strawberry"
+  }
+]
+var info = ko.toJS(data);
+
+function getToday() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+
+    today = mm +  dd  + yyyy;
+}
+
+function registerUser() {
+    var self = this;
+    self.FullName = ViewModel.fname(); 
+    self.dataPicker = ViewModel.date();
+    self.email = ViewModel.email();
+    self.isActive = ViewModel.active();
+    self.Range = ViewModel.range();
+    self.tags = ViewModel.tags();
+    debugger;
+  
+    return { fname: FullName, email: self.email, dataPicker: self.dataPicker, isActive: self.isActive, totalSum: self.Range, tags: self.tags };
+}
+ 
+
+var ViewModel = {
+    fname: ko.observable(""),
+    email: ko.observable("name@mail.com"),
+    date: ko.observable( ""),
+    tags: ko.observableArray(info[0].tags),
+    active: ko.observable(true),
+    range: ko.observable(0),
+    users: getEmUsers(),
+    addUser: function (user) {
+    
+        ViewModel.users.push( registerUser());
+
+    }
+};
+
+$(function () {
+    $('.date').datepicker({ minDate: 0 });
+});
+
+
+$(function () {
+    $('#add').click(function () { 
+            $('#fullname').val("");
+            $('#email').val("");      
+    });
+});
+
+
+
+$(function () {
+    $('#add').click(function () {
+        $('#fullname').val("");
+        $('#email').val("");
+    });
+});
+
+$(function () {
+    $("#email").focusout(function () {
+        var mail = $('#email').val();
+        var emailPattern = /^[A-Za-z0-9\u0430-\u044F\u0410-\u042F\._-]+@([A-Za-z0-9\u0430-\u044F\u0410-\u042F]{1,2}|[A-Za-z0-9\u0430-\u044F\u0410-\u042F]((?!(\.\.))[A-Za-z0-9\u0430-\u044F\u0410-\u042F.-])+[A-Za-z0-9\u0430-\u044F\u0410-\u042F])\.[A-Za-z\u0430-\u044F\u0410-\u042F]{2,}$/i;
+        if (!emailPattern.test(mail)) {
+            $('#email').val("Invalid Email, follow the pattern username@email.com");
+            $('#add').attr("disabled", true);
+        }
+        else {
+            $('#add').attr("disabled", false);
+        }
+    });
+
+});
+
+function getEmUsers() {
+    var users = [];
+   
+    $.each(info, function (index, value) {
+        var totalSum = ko.computed(function () {
+            var total = 0;
+
+            for (var i = 0; i < value.range.length; i++) {
+                total += value.range[i];
+            }
+            return total;
+        });
+        var fullName = value.name.first + " " + value.name.last;
+        users.push({ "fname": fullName, "email": value.email, "dataPicker": value.dataPicker, "isActive": value.isActive, "totalSum": totalSum, "tags": value.tags });
+        debugger;
+    });
+    
+    return new ko.observableArray(users);
+}
+
+
+
+
+
+
+
+ko.applyBindings(ViewModel);
